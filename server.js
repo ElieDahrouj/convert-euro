8b6e58d0d676a06e85d6865585ebb0098db09785
elie <edarouj@hotmail.com>
2020-04-22 15:01:50 +0200

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+
 const app = express()
 
 //here we are configuring dist to serve app files
@@ -11,4 +12,6 @@ app.get(/.*/, function (req, res) {
     res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-app.listen(8080)
+const port = process.env.PORT
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
